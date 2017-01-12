5264f19badaf5d1b815c9dee626a72773e31e109
LGCrhys <lg.christophe@gmail.com>
2017-01-12 16:46:48 +0100

    Add leaflet component

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,4 +1,4 @@
-var app = angular.module('plunker', ['nvd3', 'gridster', 'plunker.services']);
+var app = angular.module('plunker', ['nvd3', 'gridster', 'plunker.services','leaflet-directive']);
 
 app
 .controller('MainCtrl', function($scope, $timeout, DataService) {
@@ -10,69 +10,49 @@ app
 			handle: 'h3'
 		},
 		resizable: {
-     enabled: true,
-     handles: ['n', 'e', 's', 'w', 'ne', 'se', 'sw', 'nw'],
-     
-     // optional callback fired when resize is started
-     start: function(event, $element, widget) {},
-     
-     // optional callback fired when item is resized,
-     resize: function(event, $element, widget) {
-       if (widget.chart.api) widget.chart.api.update();
-     }, 
-    
-      // optional callback fired when item is finished resizing 
-     stop: function(event, $element, widget) {
-       $timeout(function(){
-         if (widget.chart.api) widget.chart.api.update();
-       },400)
-     } 
-    },
+	     enabled: true,
+	     handles: ['n', 'e', 's', 'w', 'ne', 'se', 'sw', 'nw'],
+	     
+	     // optional callback fired when resize is started
+	     start: function(event, $element, widget) {},
+	     
+	     // optional callback fired when item is resized,
+	     resize: function(event, $element, widget) {
+	       if (widget.chart.api) widget.chart.api.update();
+	     }, 
+	    
+	      // optional callback fired when item is finished resizing 
+	     stop: function(event, $element, widget) {
+	       $timeout(function(){
+	         if (widget.chart.api) widget.chart.api.update();
+	       },400)
+	     } 
+	    },
 	};
 	
 	$scope.dashboard = {
-		widgets: [{
+		widgets: [		
+		{
 			col: 0,
 			row: 0,
-			sizeY: 2,
-			sizeX: 2,
+			sizeY: 1,
+			sizeX: 1,
 			name: "Discrete Bar Chart",
 			chart: {
 			  options: DataService.discreteBarChart.options(),
 			  data: DataService.discreteBarChart.data(),
 			  api: {} 
 			}
-		}, {
-			col: 2,
+		},
+		{
+			col: 1,
 			row: 0,
-			sizeY: 2,
-			sizeX: 2,
+			sizeY: 1,
+			sizeX: 1,
 			name: "Candlestick Bar Chart",
 			chart: {
 			  options: DataService.candlestickBarChart.options(),
 			  data: DataService.candlestickBarChart.data(),
-			  api: {} 
-			}
-		}, {
-			col: 0,
-			row: 2,
-			sizeY: 2,
-			sizeX: 3,
-			name: "Line Chart",
-			chart: {
-			  options: DataService.lineChart.options(),
-			  data: DataService.lineChart.data(),
-			  api: {} 
-			}
-		}, {
-			col: 4,
-			row: 2,
-			sizeY: 1,
-			sizeX: 1,
-			name: "Pie Chart",
-			chart: {
-			  options: DataService.pieChart.options(),
-			  data: DataService.pieChart.data(),
 			  api: {} 
 			}
 		}]
@@ -101,3 +81,47 @@ app
     $scope.config.visible = true;
   }, 200);
 });
+app.controller("LeafletMapsController", [ "$scope", function($scope) {
+            angular.extend($scope, {
+                brest: {
+                    lat: 48.4000000,
+                    lng: -4.4833300,
+                    zoom: 5
+                },
+                markers: {
+                    brest: {
+	                    lat: 48.4000000,
+	                    lng: -4.4833300,
+                    }
+                },
+                layers: {
+                    baselayers: {
+                        bingAerial: {
+                            name: 'Bing Aerial',
+                            type: 'bing',
+                            key: 'Aj6XtE1Q1rIvehmjn2Rh1LR2qvMGZ-8vPS9Hn3jCeUiToM77JFnf-kFRzyMELDol',
+                            layerOptions: {
+                                type: 'Aerial'
+                            }
+                        },
+                        bingRoad: {
+                            name: 'Bing Road',
+                            type: 'bing',
+                            key: 'Aj6XtE1Q1rIvehmjn2Rh1LR2qvMGZ-8vPS9Hn3jCeUiToM77JFnf-kFRzyMELDol',
+                            layerOptions: {
+                                type: 'Road'
+                            }
+                        },
+                        bingAerialWithLabels: {
+                            name: 'Bing Aerial With Labels',
+                            type: 'bing',
+                            key: 'Aj6XtE1Q1rIvehmjn2Rh1LR2qvMGZ-8vPS9Hn3jCeUiToM77JFnf-kFRzyMELDol',
+                            layerOptions: {
+                                type: 'AerialWithLabels'
+                            }
+                        },
+
+                    }
+                }
+            });
+        }]);
