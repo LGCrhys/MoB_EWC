c95b8c33f9ad28da2dc9ca22e63ce43d23dd082f
LGCrhys <lg.christophe@gmail.com>
2017-01-12 14:59:52 +0100

    Init yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name moBEwcApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the moBEwcApp
+ */
+angular.module('moBEwcApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
