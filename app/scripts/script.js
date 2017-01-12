a907f501cc6aa6cfa560bd9867e867f7429cc015
Christophe Jollivet <christophe.jollivet@gmail.com>
2017-01-12 15:20:39 +0100

    organize the charts to reduce size

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -12,72 +12,73 @@ app
 		resizable: {
      enabled: true,
      handles: ['n', 'e', 's', 'w', 'ne', 'se', 'sw', 'nw'],
-     
+
      // optional callback fired when resize is started
      start: function(event, $element, widget) {},
-     
+
      // optional callback fired when item is resized,
      resize: function(event, $element, widget) {
        if (widget.chart.api) widget.chart.api.update();
-     }, 
-    
-      // optional callback fired when item is finished resizing 
+     },
+
+      // optional callback fired when item is finished resizing
      stop: function(event, $element, widget) {
        $timeout(function(){
          if (widget.chart.api) widget.chart.api.update();
        },400)
-     } 
+     }
     },
 	};
-	
+
 	$scope.dashboard = {
 		widgets: [{
 			col: 0,
 			row: 0,
-			sizeY: 2,
-			sizeX: 2,
+			sizeY: 1,
+			sizeX: 1,
 			name: "Discrete Bar Chart",
 			chart: {
 			  options: DataService.discreteBarChart.options(),
 			  data: DataService.discreteBarChart.data(),
-			  api: {} 
+			  api: {}
 			}
 		}, {
-			col: 2,
+			col: 1,
 			row: 0,
-			sizeY: 2,
-			sizeX: 2,
-			name: "Candlestick Bar Chart",
-			chart: {
-			  options: DataService.candlestickBarChart.options(),
-			  data: DataService.candlestickBarChart.data(),
-			  api: {} 
-			}
-		}, {
-			col: 0,
-			row: 2,
-			sizeY: 2,
-			sizeX: 3,
-			name: "Line Chart",
-			chart: {
-			  options: DataService.lineChart.options(),
-			  data: DataService.lineChart.data(),
-			  api: {} 
-			}
-		}, {
-			col: 4,
-			row: 2,
 			sizeY: 1,
 			sizeX: 1,
 			name: "Pie Chart",
 			chart: {
 			  options: DataService.pieChart.options(),
 			  data: DataService.pieChart.data(),
-			  api: {} 
+			  api: {}
+			}
+		},
+     {
+			col: 2,
+			row: 0,
+			sizeY: 1,
+			sizeX: 2,
+			name: "Candlestick Bar Chart",
+			chart: {
+			  options: DataService.candlestickBarChart.options(),
+			  data: DataService.candlestickBarChart.data(),
+			  api: {}
+			}
+		}, {
+			col: 0,
+			row: 2,
+			sizeY: 1,
+			sizeX: 2,
+			name: "Line Chart",
+			chart: {
+			  options: DataService.lineChart.options(),
+			  data: DataService.lineChart.data(),
+			  api: {}
 			}
 		}]
 	};
-  
+
   // We want to manually handle `window.resize` event in each directive.
   // So that we emulate `resize` event using $broadcast method and internally subscribe to this event in each directive
   // Define event handler
@@ -91,7 +92,7 @@ app
   angular.element(window).on('resize', function(e){
     $scope.$broadcast('resize');
   });
-  
+
   // We want to hide the charts until the grid will be created and all widths and heights will be defined.
   // So that use `visible` property in config attribute
   $scope.config = {
